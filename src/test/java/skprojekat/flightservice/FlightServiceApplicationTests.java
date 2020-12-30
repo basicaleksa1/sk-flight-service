5554f53d00355ed23b117399c27f73d85348e000
Aleksa <basicaleksa1@gmail.com>
2020-12-30 12:11:40 +0100

    First commit

---
+++ src/test/java/skprojekat/flightservice/FlightServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package skprojekat.flightservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
